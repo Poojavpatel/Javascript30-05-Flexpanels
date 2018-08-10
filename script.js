f3b9ee864b2b9a54633a43270c031824ecc23acd
Pooja patel <poojavpatel71@gmail.com>
2018-08-11 01:34:46 +0530

    js for introdiv slidein done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 /*jshint esversion: 6 */
+const introDivs = document.querySelectorAll('.projIntro');
+const projectDivs = document.querySelectorAll('.project');
+console.log(projectDivs);
+
+function itsLoaded() {
+    introDivs.forEach( (introDiv) => {
+        introDiv.classList.add('itsloaded');
+    });
+}
+
+window.addEventListener('load',itsLoaded);
