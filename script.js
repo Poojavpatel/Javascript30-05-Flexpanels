340632d75843223ae311138e297371e140fa4555
Pooja patel <poojavpatel71@gmail.com>
2018-08-11 13:58:59 +0530

    finaly figured out how to do not(this)  in vanilla js

--- script.js
+++ script.js
@@ -8,5 +8,15 @@ function itsLoaded() {
         introDiv.classList.add('itsloaded');
     });
 }
+function projSelected(event) {
+    console.log(this);
+    this.classList.add('expand');
+    const notprojs = document.querySelectorAll('.project:not(.expand)');
+    console.log(notprojs);
+    notprojs.forEach((notproj) => {notproj.classList.add('shrink');});
+}
 
 window.addEventListener('load',itsLoaded);
+projectDivs.forEach( (projectDiv) => {
+    projectDiv.addEventListener('click',projSelected);
+});
