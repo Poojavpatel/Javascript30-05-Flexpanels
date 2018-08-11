d8bcb4aaaf219b1acd0644d1660fd307b8c5fa7e
Pooja patel <poojavpatel71@gmail.com>
2018-08-11 18:45:28 +0530

    written tasks for grunt|grunt works finally

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+/*jshint esversion: 6 */
 var gulp = require('gulp');
 var csso = require('gulp-csso');
 var htmlmin = require('gulp-htmlmin');
 var runSequence = require('run-sequence');
-var uglify = require('gulp-uglify');
+// var uglify = require('gulp-uglify');
+const minify = require('gulp-minify');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('sayhello', function() {
     console.log('Hello');
@@ -19,11 +22,14 @@ gulp.task('minifycss', function () {
 
 // Gulp task to minify JavaScript files
 gulp.task('minifyjs', function() {
-    return gulp.src('./script.js')
-      // Minify the file
-      .pipe(uglify())
-      // Output
-      .pipe(gulp.dest('./build'));
+    // return gulp.src('./script.js')
+    //   // Minify the file
+    //   .pipe(uglify())
+    //   // Output
+    //   .pipe(gulp.dest('./build'));
+    gulp.src('./script.js')
+    .pipe(minify())
+    .pipe(gulp.dest('./build'));
   });
 
 // Gulp task to minify HTML files
@@ -36,11 +42,19 @@ gulp.task('minifyhtml', function() {
       .pipe(gulp.dest('./build'));
   });
 
+//Optimizing images
+gulp.task('minifyimg', () =>
+    gulp.src('./img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/img'))
+);
+
 // Gulp task to minify all files
 gulp.task('rockit', function () {
     runSequence(
       'minifyhtml',
       'minifycss',
-      'minifyjs'
+      'minifyimg',
+      'minifyjs'      
     );
   });
